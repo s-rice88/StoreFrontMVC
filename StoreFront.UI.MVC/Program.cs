9f3e2888010b5fbd175ad2447b750f62e2bb21b6
Sean Rice <sean.m.rice88@gmail.com>
2022-12-17 14:44:15 -0600

    Updated Views/Added Filter and Countdown and many many more....

--- StoreFront.UI.MVC/Program.cs
+++ StoreFront.UI.MVC/Program.cs
@@ -56,7 +56,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Products}/{action=HomePage}/{id?}");
 app.MapRazorPages();
 
 app.Run();
